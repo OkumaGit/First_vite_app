a49fb7c87e805d4b4bfae93c23e91031339083da
PromiSe#### <vicimpa@ya.ru>
2022-01-10 16:16:18 +0300

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,4 @@
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-import type { UserConfig } from 'vite';
-const config: UserConfig = {
-    plugins: [
-        tsconfigPaths(),
-    ],
-};
 
 export default defineConfig({
     base: './'
