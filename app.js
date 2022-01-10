bc888ed0f25abf57bff4d96513b3691501743406
Ruslan Nasarenko <okuma@Ruslans-iMac.local>
2022-01-10 12:46:39 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+var add = document.querySelector('add');
+var del = document.querySelector('del');
+
+console.log(add)
+
+console.log('app.js attached')
+
+function onClick() {
+  console.log('element clicked 🎉🎉🎉');
+}
+
+
+// const el = document.createElement('div');
+// el.addEventListener('click', function handleClick(event) {
+//   console.log('element clicked 🎉🎉🎉', event);
+// });
+
+// el.textContent = 'Hello world';
+// el.style.backgroundColor = 'salmon';
+// el.style.width = '150px';
+// el.style.height = '150px';
+
+// function onClick() {
+// 	console.log('Hello')
+// 	}
+
+// const box = document.getElementById('box');
+// // box.appendChild(el);
+
+
+
